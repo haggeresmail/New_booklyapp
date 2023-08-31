328ec6a01893f084ee02c01b58b881e14f4e111f
hagger <haggersoma@gmail.com>
2023-08-31 15:07:09 +0300

    fetch mewset book usecase

---
+++ lib/Features/home/Domain/use_cases/fetch_newest_book_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:bookly/Features/home/Domain/Entities/book_entity.dart';
+import 'package:bookly/Features/home/Domain/repo/home_repo.dart';
+import 'package:bookly/Features/home/Domain/use_cases/use_case.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>,NoParam>{
+final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCase(this.homeRepo);
+
+ 
+  
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param])async {
+    // TODO: implement call
+      return await homeRepo.fetchNewestBooks();
+  }
+  
+  
+}
