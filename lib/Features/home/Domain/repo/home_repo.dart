54da1cea2183ebc365b4cd6cc121834b5269e1dc
hagger <haggersoma@gmail.com>
2023-08-31 14:20:22 +0300

    use of Either class

--- lib/Features/home/Domain/repo/home_repo.dart
+++ lib/Features/home/Domain/repo/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/Features/home/Domain/Entities/book_entity.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-Future <List<BookEntity>> fetchFeaturedBooks();
-Future <List<BookEntity>> fetchNewestBooks();
+Future <Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+Future <Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
