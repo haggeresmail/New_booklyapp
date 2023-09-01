758bb8bbee26df0b03d7ecc3e4bc26c59d1e9481
hagger <haggersoma@gmail.com>
2023-09-01 13:42:33 +0300

    cache featured books

--- lib/Features/home/Data/Data_sources/home_remote_data_source.dart
+++ lib/Features/home/Data/Data_sources/home_remote_data_source.dart
@@ -1,39 +1,44 @@
 import 'package:bookly/Features/home/Data/Models/book_model/book_model.dart';
 import 'package:bookly/Features/home/Domain/Entities/book_entity.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/api_sevice.dart';
+import 'package:bookly/core/utils/functions/saveDataToHiVE.dart';
+import 'package:hive/hive.dart';
 
-abstract class HomeRemoteDataSource{
-  Future <List<BookEntity>> fetchFeaturedBooks();
-Future <List<BookEntity>> fetchNewestBooks();
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
- final ApiService apiService;
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
 
   HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() async{
-   var data =await  apiService.get(endpoint: 'volumes?Filtering=free-ebooks&q=programming');
-   List<BookEntity> books = getListBooks(data);
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getListBooks(data);
+    saveBooksData(books, kFeaturedBox);
 
-   return books;
-   
+    return books;
   }
 
   @override
-  Future<List<BookEntity>> fetchNewestBooks() async{
-     var data =await  apiService.get(endpoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=programming');
-   List<BookEntity> books = getListBooks(data);
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await apiService.get(
+        endpoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest &q=programming');
+    List<BookEntity> books = getListBooks(data);
 
-   return books;
-  }
-
-}
-List<BookEntity> getListBooks(Map<String, dynamic> data) {
-    List<BookEntity> books=[];
-    for (var bookMap in data['items']) {
-     books.add(BookModel.fromJson(bookMap));
-      
-    }
     return books;
   }
+}
+
+List<BookEntity> getListBooks(Map<String, dynamic> data) {
+  List<BookEntity> books = [];
+  for (var bookMap in data['items']) {
+    books.add(BookModel.fromJson(bookMap));
+  }
+  return books;
+}
---
+++ lib/core/utils/functions/saveDataToHiVE.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/Features/home/Domain/Entities/book_entity.dart';
+import 'package:hive/hive.dart';
+
+void saveBooksData(List<BookEntity> books, String boxName) {
+  var box = Hive.box(boxName);
+  box.addAll(books);
+}
