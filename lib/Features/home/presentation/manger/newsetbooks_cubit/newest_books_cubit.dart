91fb80ee27beec5e4b5bbb345c885b094839500c
hagger <haggersoma@gmail.com>
2023-09-05 16:38:59 +0300

    create fetch newset books method

---
+++ lib/Features/home/presentation/manger/newsetbooks_cubit/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/Domain/Entities/book_entity.dart';
+import 'package:bookly/Features/home/Domain/use_cases/fetch_newest_book_usecase.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.newsetBooksUseCase) : super(NewestBooksInitial());
+ final FetchNewestBooksUseCase newsetBooksUseCase;
+  Future<void> fetchNewestBooks()async{
+    emit( NewestBooksLoading());
+     var result = await newsetBooksUseCase.call();
+     result.fold((failure) {
+emit( NewestBooksFailure(failure.message));
+     }, (books) {
+      emit( NewestBooksSuccess(books));
+
+     });
+  }
+
+}
---
+++ lib/Features/home/presentation/manger/newsetbooks_cubit/newest_books_state.dart
@@ -0,0 +1,19 @@
+part of '../newsetbooks_cubit/newest_books_cubit.dart';
+
+@immutable
+abstract class NewestBooksState {}
+
+ class NewestBooksInitial extends NewestBooksState {}
+ class NewestBooksLoading extends NewestBooksState {}
+ class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity> books;
+
+  NewestBooksSuccess(this.books);
+
+ }
+ class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  NewestBooksFailure(this.errMessage);
+
+ }
